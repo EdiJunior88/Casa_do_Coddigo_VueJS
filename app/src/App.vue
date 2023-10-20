86808e44a49050ad4324e7ac464b250e0d28b976
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-20 20:55:41 -0300

    Sobre o Vue.js
    O que é o Vue.js?
    Onde posso aplicar o Vue.js?
    O que este livro vai abordar?
    O que são os componentes reativos?
    Qual o diferencial do Vue.js?
    A história do Vue.js
    Revisão
    Seu primeiro componente com Vue.js
    
    Instalando o Vue.js
    Escrevendo seu primeiro componente
    Revisão
    
    Construindo nossa aplicação
    Entendendo o projeto
    Iniciando o projeto

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
-import { ref } from 'vue'
-
-const msg = ref('Bem vindo a sua primeira app com Vue.js')
+import InputTask from './components/InputTask.vue'
 </script>
 
 <template>
-  <div class="hello">
-    <h1 :title="msg">{{ msg }}</h1>
-  </div>
+  <section
+    class="container mx-auto min-w-64 max-w-xl flex justify-center items-center font-['Helvetica Neue'] font-light text-sm text-slate-500 leading-snug mt-52 mb-10 relative focus:outline-none"
+  >
+    <header class="w-full">
+      <h1 class="absolute top-[-155px] w-full text-center text-8xl text-red-300 font-thin">
+        Tarefas
+      </h1>
+      <InputTask></InputTask>
+    </header> 
+  </section>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css">
+html {
+  background: #f5f5f5;
+}
+</style>
---
+++ app/src/components/InputTask.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div>
+    <input
+      class="w-full relative m-0 text-2xl leading-snug italic font-light text-slate-700 py-4 pl-14 border border-solid border-gray-200 shadow-lg rounded-md focus:outline-gray-100 focus:ring focus:ring-gray-100"
+      placeholder="O que precisa ser feito?"
+    />
+  </div>
+</template>
+
+<style lang="css" scoped></style>
