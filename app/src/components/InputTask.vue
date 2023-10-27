9f3045e214633344b3db0c102fbeef5fe62af9cb
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-26 21:12:32 -0300

    Sobre o Vue.js
    O que é o Vue.js?
    Onde posso aplicar o Vue.js?
    O que este livro vai abordar?
    O que são os componentes reativos?
    Qual o diferencial do Vue.js?
    A história do Vue.js
    Revisão
    Seu primeiro componente com Vue.js
    
    Instalando o Vue.js
    Escrevendo seu primeiro componente
    Revisão
    
    Construindo nossa aplicação
    Entendendo o projeto
    Iniciando o projeto
    Ações para nossos componentes
    (Inputs	e	eventos)
    (Outros	eventos)
    (Para saber mais)
    Revisão

--- app/src/components/InputTask.vue
+++ app/src/components/InputTask.vue
@@ -1,8 +1,20 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { Task } from '../models/Task'
+
+let task = ref(new Task())
+
+function addTask(event) {
+  task.value.completed = false
+  task.value.title = event.target.value
+  console.log(task)
+}
+</script>
 
 <template>
   <div>
     <input
+      @keyup.enter="addTask"
       class="w-full relative m-0 text-2xl leading-snug italic font-light text-slate-700 py-4 pl-14 border border-solid border-gray-200 shadow-lg rounded-md focus:outline-gray-100 focus:ring focus:ring-gray-100"
       placeholder="O que precisa ser feito?"
     />
---
+++ app/src/models/Task.js
@@ -0,0 +1,7 @@
+export function Task() {
+  return {
+    id: '',
+    title: '',
+    completed: ''
+  }
+}
