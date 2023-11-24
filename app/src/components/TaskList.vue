73b434868a0fff4b8bf194094d9a5da78b142240
EdiJunior88 <er.moura88@yahoo.com.br>
2023-11-23 21:00:10 -0300

    - Renderização Condicional As Diretivas V-IF, V-ELSE e V-ELSE-IF (Aplicando o v-if em nossa lista)

--- app/src/components/TaskList.vue
+++ app/src/components/TaskList.vue
@@ -8,12 +8,12 @@ const props = defineProps({
   todoList: Array
 })
 
-// Aqui, ref é usada para criar uma referência 
+// Aqui, ref é usada para criar uma referência
 // reativa ao array todoList.
 const todoList = ref(props.todoList)
 
-// Aqui, computed é usada para criar uma propriedade computada 
-// chamada sortedTasks. Esta propriedade retorna o array todoList 
+// Aqui, computed é usada para criar uma propriedade computada
+// chamada sortedTasks. Esta propriedade retorna o array todoList
 // ordenado alfabeticamente por título.
 const sortedTasks = computed(() => {
   let sorted = todoList
@@ -23,6 +23,10 @@ const sortedTasks = computed(() => {
     return 0
   })
 })
+
+function completeTasks(task) {
+  task.completed = !task.completed
+}
 </script>
 
 <template>
@@ -30,13 +34,51 @@ const sortedTasks = computed(() => {
     <li
       v-for="todo in sortedTasks"
       :key="todo.id"
-      class="w-full relative m-0 -mt-1 text-2xl font-normal bg-white text-slate-700 py-4 pl-14 border border-solid border-gray-200 first:border-t-0 last:border-b-0 shadow-lg rounded-b-md focus:outline-gray-100 focus:ring focus:ring-gray-100"
+      class="w-full relative m-0 -mt-1 text-2xl font-normal bg-white text-slate-700 py-4 border border-solid border-gray-200 first:border-t-0 last:border-b-0 shadow-lg rounded-b-md focus:outline-gray-100 focus:ring focus:ring-gray-100"
     >
-      <div>
-        <label>{{ todo.title }}</label>
+      <div class="flex items-center">
+        <input
+          type="checkbox"
+          class="toggle ml-4 mr-5 border border-solid border-gray-500"
+          @click="completeTasks(todo)"
+        />
+        <label v-if="todo.completed" class="todo-completed">{{ todo.title }}</label>
+        <label v-else>{{ todo.title }}</label>
       </div>
     </li>
   </ul>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.toggle {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  outline: none;
+  cursor: pointer;
+  position: relative;
+}
+
+.toggle::before {
+  content: '✔️'; /* código unicode do símbolo de check */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 14px;
+  color: transparent;
+  text-shadow: 0 0 0 rgb(11, 165, 19);
+  visibility: hidden;
+}
+
+.toggle:checked::before {
+  visibility: visible;
+}
+
+.todo-completed {
+  text-decoration: line-through;
+}
+</style>
